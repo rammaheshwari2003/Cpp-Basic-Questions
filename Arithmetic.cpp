92d01d5c8854cad17d9a488b73c41a129ecd40ed
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-08-11 17:54:03 +0530

    Addition, Subtraction, Division using Arithmetic Operator

---
+++ Arithmetic.cpp
@@ -0,0 +1,19 @@
+// Adsition, substraction, division.
+#include<iostream>
+using namespace std;
+
+int main(){
+			int num1, num2;
+			
+			cout<<"Enter the num1 and num2"<<endl;
+			
+			cin>>num1>>num2;
+			
+			cout<<"Addition of "<<num1<<" and "<<num2<<" : "<<num1+num2<<endl;
+			
+			cout<<"Substraction of "<<num1<<" and "<<num2<<" : "<<num1-num2<<endl;
+			
+			cout<<"division of "<<num1<<" and "<<num2<<" : "<<(float)num1/num2<<endl;
+	
+return 0;
+}

640fab7311b7e8ff0e5c65ffd7bf002455ddf213
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-08-11 17:57:08 +0530

    Precedence Questions

---
+++ Precedence.cpp
@@ -0,0 +1,47 @@
+// precedence Question's
+#include<iostream>
+using namespace std;
+
+int main(){
+		//1
+//		int a=12,b=10,c=1,d=4,e=2; 
+//		int x = a-b*e+c/d*a*b;  
+//		cout<<x<<endl;
+//		
+//		//2
+//		int a=12,b=10,c=1,d=4,e=2; 
+//		int x = a>b>c>d>e;  
+//		cout<<x<<endl;
+//		
+//		//3
+//		int a=12,b=10,c=1,d=4,e=2; 
+// 		int x = a<b>c<d>e<2;   
+//		cout<<x<<endl;
+//		
+//		//4
+//		int a=12,b=10,c=1,d=4,e=2; 
+// 		int x = a&b/6*4||d*4>>e; 
+// 		cout<<x<<endl;
+//
+//		//5
+//		int a=12,b=10,c=1,d=4,e=2;
+//		int x = a > b > c;  
+//		cout<<x<<endl;
+//       
+//        //6
+//		int a=12,b=10,c=1,d=4,e=2;
+//		int x = a*b-c/d+e;    
+//		cout<<x<<endl;
+//       	
+//       	//7
+//		int a=12,b=10,c=1,d=4,e=2; 
+//		int x = a*(b-c)/d+e; //note:  () get  top preference
+//		cout<<x<<endl;
+// 			 	
+// 		//8
+//		int a=12,b=10,c=1,d=4,e=2; 
+//		int x = a-b/6*4||2>>5*6;  
+//		cout<<x<<endl;
+ 
+return 0;
+}

c3751aeba97ecf2670077c2babd2db0e7fd60ffa
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-08-16 22:26:42 +0530

    1 to 20 table print

---
+++ Table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+          for(int num=1; num<=10;num++){
+					for(int i=1; i<=20; i++){
+					cout<<num<<" x "<<i<<" = "<<num*i<<endl;
+			    	}
+			  	cout<<endl;
+				  }
+
+return 0;
+}

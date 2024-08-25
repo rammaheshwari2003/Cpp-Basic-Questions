7ce64505f9d25de67677a28a4a8acb7d2f5230f8
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-08-25 10:24:22 +0530

    Question 7 Multi-Subs-Add-Div (Get Top Reference)

--- Precedence's/Q7.cpp
+++ Precedence's/Q7.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
 
-		int a=12,b=10,c=1,d=4,e=2; 
-		int x = a*(b-c)/d+e;         //note:  () get  top preference
-		cout<<x<<endl;
+	int a=12,b=10,c=1,d=4,e=2; 
+	int x = a*(b-c)+d/e;         //note:  () get  top preference
+	cout<<x<<endl;
  
 return 0;
 }

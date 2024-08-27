706abf5cb2263c3e37119cddcf5531519ce0d08c
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-08-27 20:34:12 +0530

    User Array Create

---
+++ Array's/Array_user.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cout<<"Enter a number at index: "<<i<<" :";
+	cin>>arr[i];
+}
+for(int i=0;i<n;i++){
+	cout<<"number at index "<<i<<" is : "<<arr[i]<<endl;
+}
+  return 0;
+}
+
+
+ 

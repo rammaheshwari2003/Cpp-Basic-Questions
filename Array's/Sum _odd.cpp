e3d867586701f0e750dc75f07d5ad7f0dc112399
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-08-29 21:53:12 +0530

    Sum array all odd number

---
+++ Array's/Sum _odd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+		
+			int n,sum=0;
+      cout<<"Enter the array: ";
+			cin>>n;
+			int arr[n];
+			for(int i=0; i<n; i++){
+				cout<<"Enter array element: ";
+				cin>>arr[i];
+			}
+			for(int i=0; i<n; i++){
+				cout<<"Number at index "<<i<<" is: "<<arr[i]<<endl;
+			
+			if(arr[i]%2!=0){
+				sum=sum+arr[i];
+			}
+		}
+		cout<<"Odd no. : "<<sum;
+			
+return 0;
+}

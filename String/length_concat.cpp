667167667b2bb4e8887bfb0e0eaff14049803ca9
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-09-06 23:15:54 +0530

    String length and string concatenate

---
+++ String/length_concat.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+			
+		string fruit="pineapple",new_string="";
+		cout<<fruit.length()<<endl;
+		int size=fruit.size();
+		cout<<size<<endl;
+		
+		new_string=new_string+"now";  // concatenate
+		cout<<new_string<<endl;
+		
+		new_string=new_string+" or never ";
+		cout<<new_string<<endl;
+			
+			
+
+return 0;
+}

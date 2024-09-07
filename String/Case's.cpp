4bf8c110cb3e4fda4bc8b3226143331650b9d133
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-09-07 19:37:12 +0530

    Erase, Replace, Upper Case's

---
+++ String/Case's.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+			
+// erase
+	string str1="apple is very good for health";
+	str1.erase(9,5);
+	cout<<str1<<endl;		
+	
+// replace 
+			string str2="apple is very good for health";
+			str2.replace(13,0," bad bad");
+			cout<<str2<<endl;		
+			
+// upper case
+			string str3="apple is very good for health";
+			str3[4]=toupper(str[4]);
+			cout<<str3;				
+return 0;
+}
+

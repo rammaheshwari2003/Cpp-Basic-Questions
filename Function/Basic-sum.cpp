e9c19714969b28cad36546b34fb3a6be5877e048
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-09-10 18:28:20 +0530

    Basic Sum in parameter's return

---
+++ Function/Basic-sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int add(int num1, int num2, int num3){  // formal parameter
+				return num1+num2+num3;
+			}
+
+int main(){
+			int number1, number2, number3;
+			cout<<"Enter 3 number ";
+			cin>>number1>>number2>>number3;
+			int result=add(number1, number2, number3); // actual parameter
+			cout<<"result"<<result;
+}

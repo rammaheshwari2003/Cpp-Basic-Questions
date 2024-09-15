794e98df598973c0e011ca21723789e52903f60b
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-09-15 09:37:14 +0530

    Reference , value change

---
+++ Pointer/3rd-Basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+				int number1=99, number2=67;
+				int &digit1=number1;   // reference
+				digit1=900;
+				cout<<number1;
+
+return 0;
+}
+

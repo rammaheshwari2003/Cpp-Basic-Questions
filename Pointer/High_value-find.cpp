0e4ec0aa09ca7bc762b60bc24891ebe594aa2294
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-09-20 20:40:41 +0530

    Call by Value three number find to max number is zero

---
+++ Pointer/High_value-find.cpp
@@ -0,0 +1,30 @@
+// call by value
+#include<iostream>
+using namespace std;
+					void great_zero(int *n1,int *n2, int *n3){
+				if(*n1>*n2 && *n1>*n3){
+					*n1=0;
+				}
+				else if(*n2>*n1 && *n2>*n3){
+					*n2=0;
+				}
+				else{
+					*n3=0;
+				}
+			}
+int main(){
+			// task:- wap to demontrate call by value creating two funtcion. function great_zero(), parameter = 3 int, return = void.
+			
+			
+			
+			int num1,num2,num3;
+			cout<<"Enter a Number1 , Number2 and Number3 ";
+			cin>>num1>>num2>>num3;
+			great_zero(&num1,&num2,&num3);
+			cout<<num1<<" "<<num2<<" "<<num3;
+			
+			  
+
+return 0;
+}
+ 

17768b45c58418020b9da71200c0f65e5bfd616e
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-09-30 21:53:56 +0530

    unique value in an array

--- Array's/Unique-value.cpp
+++ Array's/Unique-value.cpp
@@ -6,17 +6,27 @@ int main(){
 		int arr[]={1,2,1,5,6,9,8,5,4,6};
 		int n=sizeof(arr)/sizeof(arr[0]);
 
-		for(int i=0; i<n; i++){
-		int count=0;
-			for(int j=0; j<n; j++){
-				if(arr[i]==arr[j])
-				count++;
-			}
-			if(count==1)
-			cout<<arr[i]<<" ";
-		}
-		
+		// for(int i=0; i<n; i++){
+		// int count=0;
+		// 	for(int j=0; j<n; j++){
+		// 		if(arr[i]==arr[j])
+		// 		count++;
+		// 	}
+		// 	if(count==1)
+		// 	cout<<arr[i]<<" ";
+		// }
 
+
+		for(int i=0; i<n; i++){
+			for(int j=0; j<i; j++){
+				if(arr[i]==arr[j]){
+				break;
+			}
+			if(i==j){
+			cout<<arr[j]<<" ";
+		}
+		}
+               }
 return 0;
 }
 

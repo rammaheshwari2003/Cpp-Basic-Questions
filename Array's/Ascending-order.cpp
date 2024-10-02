d8ae4e3320feb957dd469c7553c9364f1c3b7402
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-10-02 22:51:05 +0530

    Array Ascending order

---
+++ Array's/Ascending-order.cpp
@@ -0,0 +1,24 @@
+// W.A.P to display array in ascending order ?  
+#include<iostream>
+using namespace std;
+
+int main(){
+		int arr[]={20,2,7,11,5,6,9,4,13,17,19};
+		int n=sizeof(arr)/sizeof(arr[0]);
+		
+		for(int i=0; i<n; i++){
+			for(int j=i; j<n; j++){
+				if(arr[i]>arr[j]){
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		for(int i=0; i<n; i++){
+			cout<<arr[i]<<" ";
+		}
+
+return 0;
+}
+

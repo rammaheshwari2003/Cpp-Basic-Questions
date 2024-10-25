135b6ff02d11526c591bef5566d81b3f681fe14f
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-10-25 23:17:23 +0530

    Square of any number using function.

---
+++ Function/Square.cpp
@@ -0,0 +1,16 @@
+// w.a.p to print square of any number using function.
+#include<iostream>
+using namespace std;
+	void square(int a){
+		cout<<"Square: "<<a*a;
+	}
+int main(){
+	int a;
+	cout<<"Enter a number: ";
+	cin>>a;
+	
+	square(a);
+		
+ 
+//return 0;
+}

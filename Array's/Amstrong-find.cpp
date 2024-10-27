3cbc18d7e88418fbcecd646247b24405870dbc8f
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-10-27 22:18:50 +0530

    Find amstrong number

--- Array's/Amstrong-find.cpp
+++ Array's/Amstrong-find.cpp
@@ -3,30 +3,29 @@
 using namespace std;
 
 int main(){
-				int arr[]={2,5,153,458,639,370};
-				int size=sizeof(arr)/sizeof(arr[0]);
+	int arr[]={2,5,153,458,639,370};
+	int size=sizeof(arr)/sizeof(arr[0]);
 				
-				cout<<"Original array ";
-				for(int i=0; i<size; i++){
-					cout<<arr[i]<<" ";
-				}
-				
-				cout<<"\nAmstrong number ";
-				for(int i=0; i<size; i++){
-					int num=arr[i];
-					int sum=0;
-					int temp=num;
-					while(num>0){
-						int digit=num%10;
-						sum+=digit*digit*digit;
-						num/=10;
-					}
-					if(sum==temp){
-						cout<<temp<<" ";
-					}
-				}
+	cout<<"Original array ";
+	for(int i=0; i<size; i++){
+	cout<<arr[i]<<" ";
+	}
+		
+	cout<<"\nAmstrong number ";
+	for(int i=0; i<size; i++){
+	int num=arr[i];
+	int sum=0;
+	int temp=num;
+	while(num>0){
+	int digit=num%10;
+	sum+=digit*digit*digit;
+	num/=10;
+	}
+	if(sum==temp){
+	cout<<temp<<" ";
+	}
+		}
 			
-
 return 0;
 }
 

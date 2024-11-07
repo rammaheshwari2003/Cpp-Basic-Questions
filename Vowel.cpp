d75bf3634a069c99c0b890b3dc72ef4c4a237f26
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-11-07 23:13:27 +0530

    Vowel using if condition

--- Vowel.cpp
+++ Vowel.cpp
@@ -3,28 +3,22 @@ using namespace std;
 
 int main(){
 	
-			
-//	for(int i=1; i<=100; i++){
-//	  if(i%5==0 || i%7==0)
-//	  cout<<i<<" ";
-//	}
-
-		int n;
-		cout<<"Enter Number: ";
-		cin>>n;
-		
-		if(n>0){
-		for(char i='A'; i<='Z'; i++){
-			cout<<i<<" ";
-		}
+     	int n;
+	cout<<"Enter Number: ";
+	cin>>n;
+	
+	if(n>0){
+	for(char i='A'; i<='Z'; i++){
+	cout<<i<<" ";
+	  }
 	}
-		if(n<0){
-			for(char i='z'; i>='a'; i--){
-				cout<<i<<" ";
-			}
+	  if(n<0){
+	  for(char i='z'; i>='a'; i--){
+	  cout<<i<<" ";
 		}
-		if(n==0){
-			cout<<"a, e, i, o, u";
+	    }
+	   if(n==0){
+		cout<<"a, e, i, o, u";
 		}
 
 return 0;

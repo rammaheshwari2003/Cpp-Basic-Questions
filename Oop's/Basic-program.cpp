118afca71d2547d359ece9a1015075f1de115f05
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-11-30 22:07:37 +0530

    Basic-program

---
+++ Oop's/Basic-program.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Cybrom{
+	static int a;
+	public:
+//	static	void show(){
+			void show(){
+			a++;
+			cout<<a<<"\t";
+		}
+};
+int Cybrom::a=20;
+//int Cybrom::a=10;
+int main(){
+		Cybrom c;
+		c.show();
+		c.show();
+		
+		Cybrom c1;
+		c1.show();
+		c1.show();
+		
+//		Cybrom::show();
+
+return 0;
+}

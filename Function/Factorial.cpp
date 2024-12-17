bec0e713d7c1bc3c8315b1e6d801f73c917541aa
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2024-12-17 22:29:35 +0530

    Factorial

---
+++ Function/Factorial.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int f=1;
+		void fact(int n){
+			
+			for(int i=n; i>1; i--){
+				f=f*i;
+			}
+	}
+	
+	    void display(){
+			cout<<"Factorial: "<<f;
+		
+	}
+	
+int main(){
+		int n;
+		cout<<"Enter a number: ";
+		cin>>n;
+		
+		fact(n);
+		display();
+
+return 0;
+}

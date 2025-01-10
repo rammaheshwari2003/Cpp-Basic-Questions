b2c345d31e7c3c06a663cc8dc7d83aa5dd6802f7
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2025-01-10 21:20:01 +0530

    Base number , power

--- Recursion/Power.cpp
+++ Recursion/Power.cpp
@@ -4,24 +4,18 @@ using namespace std;
 
 	int pow(int b,int p){
 			if(b==0){
-				return 0;
+			return 0;
 			}
 			if(p==0){
-				return 1;
+			return 1;
 			}
 			int ans=b*pow(b,p-1);
 			return ans;
-		}
-		
-		
+			}	
 int main(){
-		
 		int b,p;
 		cout<<"Enter a base number or power : ";
-		cin>>b>>p;
-		
+		cin>>b>>p;	
 		cout<<pow(b,p);
-		
-
 return 0;
 }

da2379b1e65dcad4457e3e84d6cc96bac21ca78c
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2025-01-14 21:44:52 +0530

    squares (using multiplication) of numbers from 1 to 5 on separate lines.

---
+++ 1 to 5 - Square.cpp
@@ -0,0 +1,12 @@
+// Write a program to output the squares (using multiplication) of numbers from 1 to 5 on separate lines.
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	for(int i=1; i<=5; i++){
+	    cout<<i<<" - "<<i*i<<endl;
+	}
+return 0;
+}
